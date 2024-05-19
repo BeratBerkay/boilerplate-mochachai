2744ac86a6b91855fc4de391494d036c9d102dc3
B3RK4Y <beratberkay58@hotmail.com>
2024-05-19 15:11:17 +0000

    feat: init gitpod config (#117) Checklist:
    <!-- Please follow this checklist and put an x in each of the boxes, like this: [x]. It will ensure that our team takes your pull request seriously. -->
    
    - [ ] I have read [freeCodeCamp's contribution guidelines](https://contribute.freecodecamp.org).
    - [ ] My pull request has a [descriptive title](https://contribute.freecodecamp.org/#/how-to-open-a-pull-request?id=prepare-a-good-pr-title) (**not** a vague title like `Update index.md`)
    
    <!--If your pull request closes a GitHub issue, replace the XXXXX below with the issue number.-->
    
    Closes #XXXXX
    
    <!-- Feel free to add any additional description of changes below this line -->

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -16,70 +16,112 @@ suite('Functional Tests', function () {
         .keepOpen()
         .get('/hello')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello Guest');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Guest');
           done();
         });
     });
     // #2
-    test('Test GET /hello with your name', function (done) {
+    test('hello xwerty', function (done) {
       chai
         .request(server)
         .keepOpen()
-        .get('/hello?name=xy_z')
+        .get('/hello?name=xwerty')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello xy_z');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello xwerty');
           done();
         });
     });
     // #3
-    test('Send {surname: "Colombo"}', function (done) {
+    test('send {surname: "Colombo"}', function(done) {
+      // we setup the request for you...
       chai
         .request(server)
-        .keepOpen()
         .put('/travellers')
-
-        .end(function (err, res) {
-          assert.fail();
-
-          done();
+        /** send {surname: 'Colombo'} here **/
+        .send({ surname: 'Colombo' })
+        // .send({...})
+        .end(function(err, res) {
+          /** your tests here **/
+          assert.equal(res.status, 200, 'response status should be 200');
+          assert.equal(res.type, 'application/json', 'Response should be json');
+          assert.equal(
+            res.body.name,
+            'Cristoforo',
+            'res.body.name should be "Christoforo"'
+          );
+          assert.equal(
+            res.body.surname,
+            'Colombo',
+            'res.body.surname should be "Colombo"'
+          );
+    
+          done(); // Never forget the 'done()' callback...
         });
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
-
+      chai
+      .request(server)
+      .put('/travellers')
+      .send({ surname: 'da Verrazzano' })
+      .end(function(err, res) {
+      
+      assert.equal(res.status,200);
+      assert.equal(res.type,'application/json');
+      assert.equal(res.body.name,'Giovanni');
+      assert.equal(res.body.surname,'da Verrazzano');
+      
       done();
+    });
+      
     });
   });
 });
 
 const Browser = require('zombie');
-
-suite('Functional Tests with Zombie.js', function () {
+Browser.site = 'https://3000-freecodecam-boilerplate-1ccn3tqi60n.ws-us110.gitpod.io';
+const browser = new Browser();
+suite('Functional Tests with Zombie.js', function() {
   this.timeout(5000);
-
-
-
-  suite('Headless browser', function () {
+  
+  suiteSetup(function(done) {
+    return browser.visit('/', done);
+  });
+ 
+  suite('Headless browser', function() {
     test('should have a working "site" property', function() {
       assert.isNotNull(browser.site);
     });
   });
-
+   
   suite('"Famous Italian Explorers" form', function () {
     // #5
-    test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+    test('submit "surname" : "Colombo" - write your e2e test...', function(done) {
+      browser.fill('surname', 'Colombo').then(() => {
+        browser.pressButton('submit', () => {
+        browser.assert.success();
+        browser.assert.text('span#name', 'Cristoforo');
+        browser.assert.text('span#surname', 'Colombo');
+        browser.assert.elements('span#dates', 1);
+    
+        done(); 
+        });
+      });
     });
     // #6
-    test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+    test('submit "surname" : "Vespucci" - write your e2e test...', function(done) {
+      browser.fill('surname', 'Vespucci').then(() => {
+        browser.pressButton('submit', () => {
+        browser.assert.success();
+        browser.assert.text('span#name', 'Amerigo');
+        browser.assert.text('span#surname', 'Vespucci');
+        browser.assert.elements('span#dates', 1);
+    
+        done();
+        });
+       });
+      });
     });
-  });
 });
